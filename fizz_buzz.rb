03cdef5b047996efeafdfaed891b88450ba514c9
Sarah <nellamsem@gmail.com>
2016-12-12 22:47:49 -0800

    Add files via upload

---
+++ fizz_buzz.rb
@@ -0,0 +1,14 @@
+
+numbers = (1..100).to_a
+
+numbers.each do |number|
+  if number % 3 == 0 && number % 5 == 0
+  puts "FizzBuzz"
+  elsif number % 3 == 0
+    puts "Fizz"
+  elsif number % 5 == 0
+    puts "Buzz"
+  else
+    puts number
+  end
+end
